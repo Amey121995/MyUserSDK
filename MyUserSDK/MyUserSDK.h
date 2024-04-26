e580d2b79c0e7f85a6767045575557a7c30cb0f6
Amey121995 <amey.dalvi154@gmail.com>
2024-04-26 18:26:26 +0530

    Initial Commit

---
+++ MyUserSDK/MyUserSDK.h
@@ -0,0 +1,18 @@
+//
+//  MyUserSDK.h
+//  MyUserSDK
+//
+//  Created by Amey Dalvi on 26/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyUserSDK.
+FOUNDATION_EXPORT double MyUserSDKVersionNumber;
+
+//! Project version string for MyUserSDK.
+FOUNDATION_EXPORT const unsigned char MyUserSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyUserSDK/PublicHeader.h>
+
+
